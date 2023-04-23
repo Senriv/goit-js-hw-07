32bb4988ef5cd153d6af11e3a1e1df6471b130ee
Senriv <ikiry2013@gmail.com>
2023-04-23 22:05:25 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+<a class="gallery__link" href=${original}>
+<img
+class="gallery__image"
+src=${preview}
+data-source=${original}
+alt=${description}
+/>
+</a>
+</li>`
+  )
+  .join("");
+
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryEl);
+
+refs.galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const currentImage = event.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${currentImage}" width="800" height="600">`);
+
+  instance.show();
+
+  refs.galleryContainer.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryContainer: document.querySelector(".gallery"),
+};
+
+const listElGallery = onMakeGalleryEl(galleryItems);
+onMakeGallery(listElGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+function onMakeGalleryEl(el) {
+  return el
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>
+  </li>`
+    )
+    .join("");
+}
+
+function onMakeGallery(el) {
+  refs.galleryContainer.innerHTML = el;
+}
